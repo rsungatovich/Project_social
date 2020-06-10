dc0890930c5c11cc241ecf89158f9d13a4221ca2
Rim Sungatovich <rsungatovichsocial@gmail.com>
2020-06-10 17:49:37 +0300

    added header v.01

---
+++ components/Container.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <slot>components</slot>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.container {
+  max-width: 1440px;
+  padding: 0 60px;
+  margin: 0 auto;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,23 @@
 <template>
   <div>
+    <container>
+      <my-header />
+    </container>
     <nuxt />
+    <container> </container>
   </div>
 </template>
+
+<script>
+import Container from '@/components/Container';
+import Header from '@/components/Header';
+
+export default {
+  components: {
+    'my-header': Header,
+    container: Container,
+  },
+};
+</script>
 
 <style lang="scss" scoped></style>
