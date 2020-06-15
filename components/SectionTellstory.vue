5d0693b4d92485ef96e16f8355d207bd01ed58cf
Rim Sungatovich <rsungatovichsocial@gmail.com>
2020-06-15 14:55:29 +0300

    added responsive/section-tellstory v.02

--- components/SectionTellstory.vue
+++ components/SectionTellstory.vue
@@ -1,52 +1,47 @@
 <template>
   <section class="section-tellstory">
-    <window class="section-tellstory__window">
-      <ui-title class="section-tellstory__title">
-        Расскажите свою историю
-      </ui-title>
-      <div class="section-tellstory__container">
-        <ui-subtitle class="section-tellstory__subtitle">
-          Мы публикуем новые истории на сайте раз в неделю. Есть 2 варианта
-          поделиться своей историей неизлечимых привычек, навязчивых идей и
-          болезненных привязанностей.
-        </ui-subtitle>
-        <div class="section-tellstory__box">
-          <div class="section-tellstory__controls">
-            <button
-              class="section-tellstory__control section-tellstory__control_is-active"
-            >
-              1-й вариант
-            </button>
-            <button class="section-tellstory__control">
-              2-й вариант
-            </button>
-          </div>
-          <div class="section-tellstory__inner">
-            <p class="section-tellstory__description">
-              Заполнить подробную форму прямо на сайте и мы опубликуем вашу
-              историю после проверки. Пожалуйста, заполняйте все пункты
-              корректно, если вы испытаете какие-то сложности, воспользуйтесь
-              2-м вариантом.
-            </p>
-            <middle-button>
-              Заполнить форму
-            </middle-button>
-          </div>
+    <ui-title class="section-tellstory__title">
+      Расскажите свою историю
+    </ui-title>
+    <div class="section-tellstory__container">
+      <ui-subtitle class="section-tellstory__subtitle">
+        Мы публикуем новые истории на сайте раз в неделю. Есть 2 варианта
+        поделиться своей историей неизлечимых привычек, навязчивых идей и
+        болезненных привязанностей.
+      </ui-subtitle>
+      <div class="section-tellstory__box">
+        <div class="section-tellstory__controls">
+          <button
+            class="section-tellstory__control section-tellstory__control_is-active"
+          >
+            1-й вариант
+          </button>
+          <button class="section-tellstory__control">
+            2-й вариант
+          </button>
+        </div>
+        <div class="section-tellstory__inner">
+          <p class="section-tellstory__description">
+            Заполнить подробную форму прямо на сайте и мы опубликуем вашу
+            историю после проверки. Пожалуйста, заполняйте все пункты корректно,
+            если вы испытаете какие-то сложности, воспользуйтесь 2-м вариантом.
+          </p>
+          <middle-button class="section-tellstory__middle-button">
+            Заполнить форму
+          </middle-button>
         </div>
       </div>
-    </window>
+    </div>
   </section>
 </template>
 
 <script>
-import Window from '@/components/Window';
 import Title from '@/components/ui/Title';
 import Subtitle from '@/components/ui/Subtitle';
 import MiddleButton from '@/components/ui/MiddleButton';
 
 export default {
   components: {
-    window: Window,
     'ui-title': Title,
     'ui-subtitle': Subtitle,
     'middle-button': MiddleButton,
@@ -61,7 +56,7 @@ export default {
 }
 
 .section-tellstory__title {
-  margin-bottom: 32px;
+  margin: 0 0 32px;
 }
 
 .section-tellstory__container {
@@ -74,7 +69,7 @@ export default {
 }
 
 .section-tellstory__controls {
-  margin-right: 40px;
+  margin: 0 40px 0 0;
 }
 
 .section-tellstory__control {
@@ -93,7 +88,7 @@ export default {
 }
 
 .section-tellstory__control:first-child {
-  margin-bottom: 10px;
+  margin: 0 0 10px;
 }
 
 .section-tellstory__description {
@@ -104,5 +99,132 @@ export default {
   font-size: 18px;
   line-height: 22px;
   color: #666666;
+}
+
+@media screen and (max-width: 1280px) {
+  .section-tellstory {
+    padding: 90px 0;
+  }
+
+  .section-tellstory__description {
+    max-width: 570px;
+  }
+}
+
+@media screen and (max-width: 1140px) {
+  .section-tellstory__controls {
+    margin: 0 30px 0 0;
+  }
+
+  .section-tellstory__description {
+    max-width: 470px;
+  }
+}
+
+@media screen and (max-width: 1024px) {
+  .section-tellstory {
+    padding: 80px 0;
+  }
+
+  .section-tellstory__title {
+    margin: 0 0 20px;
+  }
+
+  .section-tellstory__description {
+    max-width: 447px;
+    font-size: 15px;
+    line-height: 19px;
+  }
+
+  .section-tellstory__control {
+    font-size: 15px;
+    line-height: 19px;
+  }
+}
+
+@media screen and (max-width: 940px) {
+  .section-tellstory__container {
+    flex-direction: column;
+    align-items: center;
+    justify-content: unset;
+  }
+
+  .section-tellstory__box {
+    flex-direction: column;
+  }
+
+  .section-tellstory__controls {
+    margin: 0 0 30px;
+    display: flex;
+  }
+
+  .section-tellstory__title {
+    max-width: 380px;
+    margin: 0 auto 26px;
+  }
+
+  .section-tellstory__subtitle {
+    max-width: 380px;
+    margin: 0 0 80px;
+  }
+
+  .section-tellstory__description {
+    max-width: 380px;
+    margin: 0 0 50px;
+  }
+
+  .section-tellstory__control:first-child {
+    margin: 0 30px 0 0;
+  }
+
+  .section-tellstory__control_is-active {
+    border-bottom: 1.5px solid $mainColor;
+  }
+}
+
+@media screen and (max-width: 425px) {
+  .section-tellstory {
+    padding: 50px 0;
+  }
+
+  .section-tellstory__container {
+    align-items: unset;
+  }
+
+  .section-tellstory__controls {
+    margin: 0 0 20px;
+    display: flex;
+  }
+
+  .section-tellstory__title {
+    margin: 0 0 16px;
+  }
+
+  .section-tellstory__subtitle {
+    max-width: 300px;
+    margin: 0 0 40px;
+  }
+
+  .section-tellstory__control {
+    font-size: 13px;
+    line-height: 19px;
+  }
+
+  .section-tellstory__description {
+    margin: 0 0 30px;
+    font-size: 13px;
+    line-height: 16px;
+  }
+
+  .section-tellstory__control:first-child {
+    margin: 0 20px 0 0;
+  }
+}
+
+@media screen and (max-width: 375px) {
+  .section-tellstory__middle-button {
+    margin: 0 auto;
+    display: block;
+  }
 }
 </style>
