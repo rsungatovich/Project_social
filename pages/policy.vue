076049611edd9e03ceeb7b83385641d5f2bb54a4
Rim Sungatovich <rsungatovichsocial@gmail.com>
2020-06-17 17:02:19 +0300

    added section-policy v.01

---
+++ pages/policy.vue
@@ -0,0 +1,22 @@
+<template>
+  <main class="content">
+    <section-policy class="content__section-policy" />
+  </main>
+</template>
+
+<script>
+import SectionPolicy from '@/components/SectionPolicy';
+
+export default {
+  components: {
+    'section-policy': SectionPolicy,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.content__section-policy {
+  margin: 0 auto;
+  font-family: 'Inter', monospace;
+}
+</style>
