4d2339a94e1bdc8b7e2643cbe92f40617d3aafc4
Rim Sungatovich <rsungatovichsocial@gmail.com>
2020-06-19 14:32:21 +0300

    added ui-no-found-404

--- pages/policy.vue
+++ pages/policy.vue
@@ -1,15 +1,18 @@
 <template>
   <main class="content">
     <section-policy class="content__section-policy" />
+    <!-- <ui-no-found-404 class="content__no-found-404" /> -->
   </main>
 </template>
 
 <script>
 import SectionPolicy from '@/components/SectionPolicy';
+import NoFound404 from '@/components/ui/NoFound404';
 
 export default {
   components: {
     'section-policy': SectionPolicy,
+    'ui-no-found-404': NoFound404,
   },
 };
 </script>
@@ -18,5 +21,9 @@ export default {
 .content__section-policy {
   margin: 0 auto;
   font-family: 'Inter', monospace;
+}
+
+.content__no-found-404 {
+  margin: 0 auto;
 }
 </style>
--- pages/stories.vue
+++ pages/stories.vue
@@ -1,15 +1,18 @@
 <template>
   <main class="content">
     <section-allstories class="content__section-allstories" />
+    <!-- <ui-no-found-404 class="content__no-found-404" /> -->
   </main>
 </template>
 
 <script>
 import SectionAllstories from '@/components/SectionAllstories';
+import NoFound404 from '@/components/ui/NoFound404';
 
 export default {
   components: {
     'section-allstories': SectionAllstories,
+    'ui-no-found-404': NoFound404,
   },
 };
 </script>
@@ -20,6 +23,10 @@ export default {
 }
 
 .content__section-allstories {
+  margin: 0 auto;
+}
+
+.content__no-found-404 {
   margin: 0 auto;
 }
 </style>
